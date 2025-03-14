172165b445021ada1bea19f78dc051c53f88c8c2
Pabin R Luitel <pabin2050@gmail.com>
2025-03-14 15:19:31 -0500

    feat: add blog post edit and delete API

--- backend/src/controllers/blogControllers.ts
+++ backend/src/controllers/blogControllers.ts
@@ -51,3 +51,54 @@ export const createBlogPosts: RequestHandler<
     }
   }
 };
+
+export const editBlogPosts: RequestHandler<
+  { postId: string },
+  Partial<BlogPost>,
+  Partial<BlogPost>
+> = async (req, res, next) => {
+  const { postId } = req.params;
+  const data: Partial<BlogPost> = req.body;
+
+  try {
+    const postsData = await fs.readFile(filePath, "utf8");
+    const blogPost: BlogPost[] = JSON.parse(postsData);
+
+    const updatedPosts = blogPost.map((post) =>
+      post.id === postId ? { ...post, ...data } : post
+    );
+
+    try {
+      await fs.writeFile(filePath, JSON.stringify(updatedPosts));
+      return res.json(data);
+    } catch (err) {
+      next(err);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteBlogPosts: RequestHandler<
+  { postId: string },
+  { success: boolean },
+  unknown
+> = async (req, res, next) => {
+  const { postId } = req.params;
+
+  try {
+    const postsData = await fs.readFile(filePath, "utf8");
+    const blogPost: BlogPost[] = JSON.parse(postsData);
+
+    const filteredPosts = blogPost.filter((post) => post.id !== postId);
+    try {
+      await fs.writeFile(filePath, JSON.stringify(filteredPosts));
+      return res.json({ success: true });
+    } catch (err) {
+      next(err);
+    }
+  } catch (err) {
+    next(err);
+  }
+  return res.json({ success: false });
+};
--- backend/src/routes/BlogRoutes.ts
+++ backend/src/routes/BlogRoutes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { createBlogPosts, getBlogPosts } from "../controllers/blogControllers";
+import {
+  createBlogPosts,
+  deleteBlogPosts,
+  editBlogPosts,
+  getBlogPosts,
+} from "../controllers/blogControllers";
 
 export const blogRoutes = Router();
 
 blogRoutes.get("/", getBlogPosts);
 blogRoutes.post("/", createBlogPosts);
+blogRoutes.put("/:postId", editBlogPosts);
+blogRoutes.delete("/:postId", deleteBlogPosts);
